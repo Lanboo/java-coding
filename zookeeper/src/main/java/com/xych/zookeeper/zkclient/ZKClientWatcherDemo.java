33426498db1aedb785ec169011f78dfa3f7d736e
Lanboo <549434510@qq.com>
2018-07-02 23:41:36 +0800

    ADD Zookeeper ZkClient Watcher事件的代码

--- zookeeper/src/main/java/com/xych/zookeeper/zkclient/ZKClientWatcherDemo.java
+++ zookeeper/src/main/java/com/xych/zookeeper/zkclient/ZKClientWatcherDemo.java
@@ -1,7 +1,11 @@
 package com.xych.zookeeper.zkclient;
 
+import java.io.IOException;
+
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.ZkConnection;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
 
 import com.xych.zookeeper.zkclient.listener.ChildListener;
 import com.xych.zookeeper.zkclient.listener.DataListener;
@@ -18,17 +22,67 @@ public class ZKClientWatcherDemo
     public static final String CONNECTSTRING = "192.168.27.133:2181";
     private static ZkClient zkClient;
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        connect();
-        addListener();
+        connect();// 创建连接
+        addListener();// 添加listener
+        createNode();// 创建节点 
+        editNode();// 修改节点
+        createChildNode();// 添加子节点
+        editChildNode();// 修改子节点
+        deleteChildNode();
+        deleteNode();// 删除节点
+    }
+
+    private static void deleteChildNode()
+    {
+        boolean b = zkClient.delete("/xych/child", -1);
+        log.info("是否删除/xych/child：{}", b);
+    }
+
+    private static void editChildNode()
+    {
+        User user = new User("xych2", 24);
+        Stat stat = zkClient.writeDataReturnStat("/xych/child", user, -1);
+        log.info("修改子节点/xych/child：{}", stat.toString().trim());
+    }
+
+    private static void createChildNode()
+    {
+        User user = new User("child", 3);
+        String path = zkClient.create("/xych/child", user, CreateMode.EPHEMERAL);
+        log.info("创建子节点：{}", path);
+    }
+
+    private static void deleteNode()
+    {
+        boolean b = zkClient.delete("/xych", -1);
+        log.info("是否删除/xych：{}", b);
+    }
+
+    private static void editNode()
+    {
+        User user = new User("xych2", 24);
+        Stat stat = zkClient.writeDataReturnStat("/xych", user, -1);
+        log.info("修改节点/xych：{}", stat.toString().trim());
+    }
+
+    private static void createNode()
+    {
+        User user = new User("xych", 24);
+        String path = zkClient.create("/xych", user, CreateMode.PERSISTENT);//临时节点不能添加字节点
+        log.info("创建节点：{}", path);
     }
 
     private static void addListener()
     {
         zkClient.subscribeStateChanges(new StateListener());
-        zkClient.subscribeDataChanges("/xych", new DataListener());
-        zkClient.subscribeChildChanges("/xych", new ChildListener());
+        DataListener dataListener = new DataListener();
+        ChildListener childListener = new ChildListener();
+        zkClient.subscribeDataChanges("/xych", dataListener);
+        zkClient.subscribeChildChanges("/xych", childListener);
+        zkClient.subscribeDataChanges("/xych/child", dataListener);
+        zkClient.subscribeChildChanges("/xych/child", childListener);
     }
 
     private static void connect()
