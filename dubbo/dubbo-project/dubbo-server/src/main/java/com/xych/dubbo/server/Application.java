106d709fef126c2c2528427b2798a78a75bf60c7
Lanboo <549434510@qq.com>
2018-08-06 23:54:00 +0800

    ADD dubbo coding...

--- dubbo/dubbo-project/dubbo-server/src/main/java/com/xych/dubbo/server/Application.java
+++ dubbo/dubbo-project/dubbo-server/src/main/java/com/xych/dubbo/server/Application.java
@@ -1,8 +1,19 @@
 package com.xych.dubbo.server;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Application
 {
-    public static void main(String[] args)
+    private static String PATH = "dubbo/dubbb*.xml";
+
+    public static void main(String[] args) throws Exception
     {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(PATH);
+        log.info("context={}", context);
+        log.info("userService={}", context.getBean("userService"));
+        System.in.read();//阻塞主线程
     }
 }
