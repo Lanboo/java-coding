c476b298de46b9ed9e9f61f0da33f1418a527f00
Lanboo <549434510@qq.com>
2019-08-16 17:09:51 +0800

    INIT Kafka java-maven-project

---
+++ mq/kafka/kafka/src/main/java/com/xych/kafka/App.java
@@ -0,0 +1,13 @@
+package com.xych.kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
