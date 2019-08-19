66572f6114f2bc68678559a80294b7ef96beb568
Lanboo <549434510@qq.com>
2019-08-19 23:04:29 +0800

    ADD Kafka spring coding

---
+++ mq/kafka/kafka/src/main/java/com/xych/kafka/springboot/KafkaConsumerDemo.java
@@ -0,0 +1,17 @@
+package com.xych.kafka.springboot;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class KafkaConsumerDemo {
+    @KafkaListener(topics = {"test"})
+    public void listen(ConsumerRecord<Integer, String> record) throws Exception {
+        System.out.printf("topic=%s,offset=%d,value=%s\n", record.topic(), record.offset(), record.value());
+        log.info("topic={},offset={},value={}", record.topic(), record.offset(), record.value());
+    }
+}
