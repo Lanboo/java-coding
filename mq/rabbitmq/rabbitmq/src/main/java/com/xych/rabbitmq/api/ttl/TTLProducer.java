e4ec5684403bb2dd1663fd2e059eb0fdfe60662a
Lanboo <549434510@qq.com>
2019-09-06 16:33:50 +0800

    ADD RabbitMQ project ttl

---
+++ mq/rabbitmq/rabbitmq/src/main/java/com/xych/rabbitmq/api/ttl/TTLProducer.java
@@ -0,0 +1,32 @@
+package com.xych.rabbitmq.api.ttl;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.AMQP;
+
+public class TTLProducer extends BaseRabbitMQ {
+    public static void main(String[] args) throws Exception {
+        new TTLProducer().run();
+        Thread.sleep(1000);
+    }
+    
+    @Override
+    protected void doRun() {
+        try {
+            String msg = "This is TTL msg!";
+            // 对每条消息设置过期时间
+            AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()//
+                    .deliveryMode(2) // 持久化消息
+                    .contentEncoding("UTF-8") //
+                    .expiration("10000") // TTL
+                    .build();
+            // 发送消息
+            // 默认交换机，routingKey填写队列名称
+            channel.basicPublish("", QUEUE_NAME, properties, msg.getBytes());
+            System.out.println(msg);
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
